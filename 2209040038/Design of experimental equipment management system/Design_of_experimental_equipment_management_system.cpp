551564a9f2432be5e21c95568f34403ea3e1d205
Nananxuan <1033850073@qq.com>
2023-09-01 18:25:16 +0800

    commit 是否还有冲突

--- 2209040038/Design of experimental equipment management system/Design_of_experimental_equipment_management_system.cpp
+++ 2209040038/Design of experimental equipment management system/Design_of_experimental_equipment_management_system.cpp
@@ -1,21 +1,23 @@
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-// 定义设备结构体
+#define MAX_DEVICES 100
+
+// 设备信息结构体
 typedef struct {
     int id;
     char type[50];
     char name[50];
     double price;
-    char purchaseDate[20];
+    char purchaseDate[11];
     int isScrapped;
-    char scrappedDate[20];
+    char scrappedDate[11];
 } Device;
 
 // 全局变量
-Device devices[100]; // 最大设备数量为100
-int count = 0; // 当前设备数量
+Device devices[MAX_DEVICES];
+int count = 0;
 
 // 函数声明
 void displayMenu();
@@ -28,7 +30,6 @@ void searchDevice();
 int main() {
     int choice;
 
-    // 主循环
     while (1) {
         displayMenu();
         printf("请输入选择：");
@@ -52,12 +53,11 @@ int main() {
                 break;
             case 0:
                 printf("感谢使用实验设备管理系统！\n");
-                exit(0);
+                return 0;
             default:
                 printf("无效的选择，请重新输入。\n");
         }
     }
-    return 0;
 }
 
 // 显示菜单
@@ -74,30 +74,38 @@ void displayMenu() {
 
 // 录入设备信息
 void addDevice() {
-    if (count >= 100) {
+    if (count == MAX_DEVICES) {
         printf("设备数量已达上限，无法录入更多设备。\n");
         return;
     }
 
     Device device;
+
     printf("请输入设备编号：");
     scanf("%d", &device.id);
+
     printf("请输入设备种类：");
     scanf("%s", device.type);
+
     printf("请输入设备名称：");
     scanf("%s", device.name);
+
     printf("请输入设备价格：");
     scanf("%lf", &device.price);
-    printf("请输入设备购入日期：");
+
+    printf("请输入设备购入日期（YYYY-MM-DD）：");
     scanf("%s", device.purchaseDate);
-    printf("该设备是否报废（1-是，0-否）：");
+
+    printf("是否报废（1-是，0-否）：");
     scanf("%d", &device.isScrapped);
+
     if (device.isScrapped == 1) {
-        printf("请输入设备报废日期：");
+        printf("请输入设备报废日期（YYYY-MM-DD）：");
         scanf("%s", device.scrappedDate);
     }
 
     devices[count++] = device;
+
     printf("设备录入成功。\n");
 }
 
@@ -112,24 +120,26 @@ void modifyDevice() {
 
     for (i = 0; i < count; i++) {
         if (devices[i].id == id) {
-            Device device;
-
             printf("请输入设备种类：");
-            scanf("%s", device.type);
+            scanf("%s", devices[i].type);
+
             printf("请输入设备名称：");
-            scanf("%s", device.name);
+            scanf("%s", devices[i].name);
+
             printf("请输入设备价格：");
-            scanf("%lf", &device.price);
-            printf("请输入设备购入日期：");
-            scanf("%s", device.purchaseDate);
-            printf("该设备是否报废（1-是，0-否）：");
-            scanf("%d", &device.isScrapped);
-            if (device.isScrapped == 1) {
-                printf("请输入设备报废日期：");
-                scanf("%s", device.scrappedDate);
+            scanf("%lf", &devices[i].price);
+
+            printf("请输入设备购入日期（YYYY-MM-DD）：");
+            scanf("%s", devices[i].purchaseDate);
+
+            printf("是否报废（1-是，0-否）：");
+            scanf("%d", &devices[i].isScrapped);
+
+            if (devices[i].isScrapped == 1) {
+                printf("请输入设备报废日期（YYYY-MM-DD）：");
+                scanf("%s", devices[i].scrappedDate);
             }
 
-            devices[i] = device;
             found = 1;
             printf("设备信息修改成功。\n");
             break;
@@ -152,11 +162,14 @@ void classifyDevices() {
     for (i = 0; i < count; i++) {
         if (strcmp(devices[i].type, "微机") == 0) {
             computerCount++;
-        } else if (strcmp(devices[i].type, "打印机") == 0) {
+        }
+        else if (strcmp(devices[i].type, "打印机") == 0) {
             printerCount++;
-        } else if (strcmp(devices[i].type, "扫描仪") == 0) {
+        }
+        else if (strcmp(devices[i].type, "扫描仪") == 0) {
             scannerCount++;
-        } else {
+        }
+        else {
             otherCount++;
         }
     }
@@ -181,9 +194,10 @@ void processDamagedDevices() {
         if (devices[i].id == id) {
             if (devices[i].isScrapped == 1) {
                 printf("设备已报废。\n");
-            } else {
+            }
+            else {
                 devices[i].isScrapped = 1;
-                printf("请输入设备报废日期：");
+                printf("请输入设备报废日期（YYYY-MM-DD）：");
                 scanf("%s", devices[i].scrappedDate);
                 printf("设备报废处理成功。\n");
                 found = 1;
